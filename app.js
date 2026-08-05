926a56c037b1076a4fb63dd16151264341e3ba86
luckywitantra <iceman.sum41@gmail.com>
2026-08-05 20:58:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,7 @@
     };
 })();
 
-const API_URL = "https://script.google.com/macros/s/AKfycbz7d6pXKgKNjHgYoZquJhIQ0rjG4xd-G5mUOdI6w7yDU9s4WXgc4VoAoUG8GX54TDUe9g/exec"; // <-- GANTI DENGAN URL API ANDA
+const API_URL = "https://script.google.com/macros/s/AKfycbxuk1xqgB2a3Aw7IvFQWvowtb1ExeNKy3bel8SFaq9asAEV-98_9UiY7tVljxkVOchNcA/exec"; // <-- GANTI DENGAN URL API ANDA
 
 /* ========================================== */
 /* 1. MESIN VIRTUAL KEYBOARD (ENTERPRISE OSK) */
